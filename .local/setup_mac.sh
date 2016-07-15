453f7b55473f48ec40e96b632401b0dcbe20b0db
x007007007 <x007007007@126.com>
2016-07-15 16:37:30 +0800

    rm mac emacs

--- .local/setup_mac.sh
+++ .local/setup_mac.sh
@@ -1,4 +1,22 @@
-sudo rm /usr/bin/emacs
-sudo rm -rf /usr/share/emacs
+#!/bin/bash
 
+if [ -x "/usr/bin/emacsclient" ] ; then
+    vstr=`/usr/bin/emacsclient --version`
+    
+    v=${vstr##* }
+    if [[ ${v%%.*} -le 24 ]] ; then
+        echo "emace version too old $vstr ,delete" 
+        sudo rm -i /usr/bin/emacsclient
+    fi
+fi
+
+if [ -x "/usr/bin/emaces" ] ; then
+    vstr=`/usr/bin/emaces --version|head -n1`
+    v=${vstr##* }
+    if [[ ${v%%.*} -le 24 ]] ; then
+        echo "emace version too old $vstr ,delete" 
+        sudo rm -i /usr/bin/emacs
+        sudo rm -i -rf /usr/share/emacs
+    fi
+fi
 
